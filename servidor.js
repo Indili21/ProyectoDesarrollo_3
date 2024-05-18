075038c6c2d1e5f6ec7fec30cc6b4df4e1b18dd8
Indili21 <indianalizettv@gmail.com>
2024-05-17 19:52:40 -0600

    actualización

--- servidor.js
+++ servidor.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000;
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static/inicio.html'));
+  res.sendFile(path.join(__dirname, 'static/HTML/inicio.html'));
 });
 
 app.get('/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static/inicio.html'));
+  res.sendFile(path.join(__dirname, 'static/HTML/inicio.html'));
 });
 
 app.listen(port, () => {
